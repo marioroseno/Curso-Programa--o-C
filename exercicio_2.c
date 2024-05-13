937be6138e52e76fcad4ce8d027971540ba45332
devmaroba <devmaroba@gmail.com>
2024-05-13 20:34:13 -0300

    exercicio_2

---
+++ exercicio_2.c
@@ -0,0 +1,23 @@
+//Faça um programa em C para trocar o valor de duas variáveis inteiras sem utilizar nenhuma
+//variável auxiliar.
+
+#include <stdio.h>
+
+int main(){
+
+    int a = 10;
+    int b = 20;
+
+    printf("\nO valor de A e: %d", a);
+
+    printf("\nE o valor de B e: %d", b);
+
+    a = 20;
+    b = 10;
+
+    printf("\nAgora o valor de A e: %d", a);
+
+    printf("\nE o valor de B e: %d", b);
+
+
+}
